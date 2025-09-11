6b121dc2274da9c1cd2790b3791c56479e187b4d
lilong <lilong.qian@advancegroup.com>
2025-09-11 11:13:17 +0800

    feat: update readme.md

--- run2.sh
+++ run2.sh
@@ -1,3 +1,3 @@
 config='configs/ktp_flux_kontext_fp16yaml'
-python3 -m src.main --config $config --cache
-# CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file accelerate_config.yaml -m src.main --config $config
+# python3 -m src.main --config $config --cache
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file accelerate_config.yaml -m src.main --config $config
