acf43ad315b7f86142b4a0f95a504e2de22ad3c1
lilong.qian <lilong.qian@advancegroup.com>
2025-09-19 13:43:14 +0800

    feat: update settings for fsdp [default config]

--- run2.sh
+++ run2.sh
@@ -5,5 +5,13 @@ config='/mnt/nas/public2/lilong/repos/qwen-image-finetune/tests/test_configs/tes
 # config='tests/test_configs/test_example_qwen_image_edit_fp16.yaml'
 # python3 -m src.main --config $config --cache
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-CUDA_VISIBLE_DEVICES=1,2 accelerate launch --config_file accelerate_config.yaml -m src.main --config $config
+# export NCCL_ASYNC_ERROR_HANDLING=1
+# export NCCL_DEBUG=INFO
+# 仅排查时用（会让错尽快抛出）：
+# export TORCH_NCCL_BLOCKING_WAIT=1
+# 看到过 torch/_compile 参与就禁用 Dynamo：
+# export TORCHDYNAMO_DISABLE=1
+# 对 A100/H100 常能降低抢占抖动：
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+CUDA_VISIBLE_DEVICES=0,1,2 accelerate launch --config_file accelerate_config.yaml -m src.main --config $config
 
--- src/trainer/base_trainer.py
+++ src/trainer/base_trainer.py
@@ -13,13 +13,16 @@ import torch.nn as nn
 import os
 import shutil
 import json
+from functools import partial
 import numpy as np
 from src.utils.sampling import calculate_shift, retrieve_timesteps
 from tqdm import tqdm
 from diffusers.utils import convert_state_dict_to_diffusers
 from diffusers.utils.torch_utils import is_compiled_module
 from peft.utils import get_peft_model_state_dict
-
+from torch.backends.cuda import sdp_kernel
+from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy  # transformer_auto_wrap_policy
+from torch.distributed.fsdp import ShardingStrategy, BackwardPrefetch
 import logging
 import PIL
 import signal
@@ -175,33 +178,9 @@ class BaseTrainer(ABC):
 
     def accelerator_prepare(self, train_dataloader):
         """Prepare accelerator"""
-        from diffusers.loaders import AttnProcsLayers
-        from src.utils.lora_utils import get_lora_layers
-        lora_layers_model = AttnProcsLayers(get_lora_layers(self.dit))
-        print('lora number of parameters')
-        n_p = 0
-        i = 0
-        for name, param in lora_layers_model.named_parameters():
-            i += 1
-            if i <= 5:
-                print(name, param.shape)
-            n_p += param.numel()
-        print(f'lora number of parameters: {n_p}')
-        p_trainable = 0
-        i = 0
-        for name, param in self.dit.named_parameters():
-            i += 1
-            if param.requires_grad:
-                p_trainable += param.numel()
-                if 'lora' not in name:
-                    print('non-lora got grad', name, param.numel())
-                else:
-                    if i<= 5:
-                        print(name, param.shape)
-            else:
-                if 'lora' in name:
-                    print('lora got no grad', name, param.numel())
-        print(f"trainable number of parameters: {p_trainable}")
+        # from diffusers.loaders import AttnProcsLayers
+        # from src.utils.lora_utils import get_lora_layers
+        # lora_layers_model = AttnProcsLayers(get_lora_layers(self.dit))
         # 根据配置决定是否启用梯度检查点
         if self.config.train.gradient_checkpointing:
             self.dit.enable_gradient_checkpointing()
@@ -215,58 +194,62 @@ class BaseTrainer(ABC):
             )
             logging.info(f"Loaded optimizer and scheduler from {self.config.resume}")
 
+        # sdp_kernel(enable_flash=False, enable_math=False, enable_mem_efficient=True)
+
         plug = getattr(self.accelerator.state, "fsdp_plugin", None)
         if plug is not None:
             from torch.distributed.fsdp import BackwardPrefetch, MixedPrecision
             import torch
-            torch.backends.cuda.enable_flash_sdp(False)
+            # torch.backends.cuda.enable_flash_sdp(False)
+            torch.backends.cuda.enable_flash_sdp(True)
+
             torch.backends.cuda.enable_mem_efficient_sdp(True)
             torch.backends.cuda.enable_math_sdp(False)
 
             plug.use_orig_params = True
             plug.ignored_modules = collect_lora_linears(self.dit)
-
             plug.limit_all_gathers = True
-            plug.forward_prefetch = False
-            # plug.backward_prefetch = BackwardPrefetch.BACKWARD_PRE  # 边回传边预取
-            plug.backward_prefetch = BackwardPrefetch.BACKWARD_POST
-
+            # plug.forward_prefetch = False
+            # plug.backward_prefetch = BackwardPrefetch.BACKWARD_POST
+            plug.forward_prefetch = True
+            plug.backward_prefetch = BackwardPrefetch.BACKWARD_PRE  # 边回传边预取
             plug.sync_module_states = True        # 主卡广播初始化，避免不一致
-            plug.min_num_params = 5_000_000
-
+            plug.min_num_params = 20_000_000  #5_000_000
             plug.mixed_precision = MixedPrecision(
                     param_dtype=torch.bfloat16,
                     reduce_dtype=torch.bfloat16,
                     buffer_dtype=torch.bfloat16,
                     cast_forward_inputs=False,
                 )
-            from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy, size_based_auto_wrap_policy
-            from torch.distributed.fsdp import ShardingStrategy, BackwardPrefetch
-
-            from src.models.transformer_qwenimage import QwenImageTransformerBlock  # 你的类路径
-            from functools import partial
-
+            # from src.models.transformer_qwenimage import QwenImageTransformerBlock  # 你的类路径
             # plug.auto_wrap_policy = partial(
             #     transformer_auto_wrap_policy,
             #     transformer_layer_cls={QwenImageTransformerBlock},  # 注意：是关键字参数
             # )
-            plug.auto_wrap_policy = partial(size_based_auto_wrap_policy, min_num_params=10_000_000)
-
+            plug.auto_wrap_policy = partial(size_based_auto_wrap_policy, min_num_params=plug.min_num_params)
             # from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
-            plug.sharding_strategy = ShardingStrategy.FULL_SHARD  #SHARD_GRAD_OP
-            from torch.backends.cuda import sdp_kernel
-            sdp_kernel(enable_flash=False, enable_math=False, enable_mem_efficient=True)
+            plug.sharding_strategy = ShardingStrategy.FULL_SHARD #FULL_SHARD   # SHARD_GRAD_OP
 
             self.dit = self.dit.to('cpu')
             torch.cuda.empty_cache()
             import gc
             gc.collect()
 
-        self.dit, optimizer, train_dataloader, lr_scheduler = (
-            self.accelerator.prepare(
-                self.dit, self.optimizer, train_dataloader, self.lr_scheduler
+            self.dit, optimizer, train_dataloader, lr_scheduler = (
+                self.accelerator.prepare(
+                    self.dit, self.optimizer, train_dataloader, self.lr_scheduler
+                )
             )
-        )
+        else:
+            from diffusers.loaders import AttnProcsLayers
+            from src.utils.lora_utils import get_lora_layers
+            lora_layers_model = AttnProcsLayers(get_lora_layers(self.dit))
+            lora_layers_model, optimizer, train_dataloader, lr_scheduler = (
+                self.accelerator.prepare(
+                    lora_layers_model, self.optimizer, train_dataloader, self.lr_scheduler
+                )
+            )
+            self.dit = self.dit.to(self.accelerator.device)
         self.optimizer = optimizer
         self.lr_scheduler = lr_scheduler
 
@@ -662,7 +645,9 @@ class BaseTrainer(ABC):
 
             state_info = {"global_step": global_step, "epoch": epoch, "is_last": is_last}
             if is_last:
-                self.accelerator.save_state(save_path)
+                plug = self.accelerator.state.fsdp_plugin if hasattr(self.accelerator.state, 'fsdp_plugin') else None
+                if plug is None:
+                    self.accelerator.save_state(save_path)  # when save in fsdp, will cause problem
                 git_info = get_git_info()
                 state_info.update(git_info)
             with open(os.path.join(save_path, "state.json"), "w") as f:
@@ -671,7 +656,7 @@ class BaseTrainer(ABC):
 
     def save_lora(self, save_folder, adapter_name=None):
         """Save LoRA weights"""
-        if not save_folder.endswith(".safetensors"):
+        if save_folder.endswith(".safetensors"):
             print(f"Warning: save_folder {save_folder} should a folder")
         if self.accelerator is not None:
             unwrapped_transformer = self.accelerator.unwrap_model(self.dit)
